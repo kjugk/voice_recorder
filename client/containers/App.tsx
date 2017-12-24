8f0af12fa72ee7c02069e5361130204afa3cd200
kjugk <k02ugk@gmail.com>
2017-12-25 00:04:49 +0900

    Playerの雛形を作成

--- client/containers/App.tsx
+++ client/containers/App.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import ArticleList from '../containers/ArticleList';
+import Player from './Player';
 
 class App extends React.Component {
   public render() {
     return (
       <React.Fragment>
         <ArticleList />
+        <Player />
       </React.Fragment>
     );
   }
