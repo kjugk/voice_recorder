641bc30ddf3965f5ce2df3533d56bf6924a07b5a
kjugk <k02ugk@gmail.com>
2018-02-10 14:02:02 +0900

    Split components into multi files.

---
+++ client/components/articles/ListItemHeadline.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import * as Types from '../../types';
+import * as moment from 'moment';
+import { formatDurationToTime } from '../../lib/Player';
+
+interface ListIteamHeadlineProps {
+  article: Types.ArticleItemState;
+}
+
+export const ListItemHeadline: React.SFC<ListIteamHeadlineProps> = (props) => {
+  const { article } = props;
+
+  return(
+    <div className="columns is-mobile ">
+      <div className="column">
+        <span className="title is-5">{article.title}</span>
+      </div>
+      <div className="column is-narrow">
+        <div>
+          <span className="is-pulled-right is-size-7">
+            {moment(article.createdAt).format('YYYY/MM/DD')}
+          </span>
+        </div>
+        <div>
+          <span className="is-pulled-right is-size-7">
+            {formatDurationToTime(article.duration)}
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
