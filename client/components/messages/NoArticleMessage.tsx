32acf5b454c454e22cb729485c5feda98cc5fd67
kjugk <k02ugk@gmail.com>
2018-02-18 17:09:29 +0900

    Change layout.

--- client/components/messages/NoArticleMessage.tsx
+++ client/components/messages/NoArticleMessage.tsx
@@ -8,7 +8,7 @@ export const NoArticleMessage: React.SFC = () => {
 
       <div className="columns" style={{marginTop: '3.8rem'}}>
         <div className="column">
-          <h5 className="title is-5">No data saved to server.</h5>
+          <h5 className="title is-5">No data saved to server</h5>
           <p>
             All your recorded data is saved into your browser storage instead of server.
             <br/>
@@ -17,32 +17,42 @@ export const NoArticleMessage: React.SFC = () => {
         </div>
         <div className="column">
           <h5 className="title is-5">Supporting browsers</h5>
-          <ul>
-            <li>
-              <span className="icon">
-                <i className="fab fa-edge" />
-              </span>
-              Latest Edge
-            </li>
-            <li>
-              <span className="icon">
-                <i className="fab fa-firefox" />
-              </span>
-              Latest Firefox
-            </li>
-            <li>
-              <span className="icon">
-                <i className="fab fa-chrome" />
-              </span>
-              Latest Chrome
-            </li>
-            <li>
-              <span className="icon">
-                <i className="fab fa-android" />
-              </span>
-              Android 7.0+ Chrome
-            </li>
-          </ul>
+          <div className="columns">
+            <div className="column">
+              <h6 className="title is-6">Desktop</h6>
+              <ul>
+                <li>
+                  <span className="icon">
+                    <i className="fab fa-edge" />
+                  </span>
+                  Latest Edge
+                </li>
+                <li>
+                  <span className="icon">
+                    <i className="fab fa-firefox" />
+                  </span>
+                  Latest Firefox
+                </li>
+                <li>
+                  <span className="icon">
+                    <i className="fab fa-chrome" />
+                  </span>
+                  Latest Chrome
+                </li>
+              </ul>
+            </div>
+            <div className="column">
+              <h6 className="title is-6">Mobile</h6>
+              <ul>
+                <li>
+                  <span className="icon">
+                    <i className="fab fa-android" />
+                  </span>
+                  Android 7+ Chrome
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </div>
