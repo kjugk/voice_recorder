ec578c941b0dc8beee3de548a9d5679a912d35a8
Koji Uegaki <kjugkjk@gmail.com>
2018-02-27 19:02:13 +0900

    Replace server to typescript

---
+++ server/routes/index.ts
@@ -0,0 +1,22 @@
+import * as express from 'express';
+const router = express.Router();
+import * as fs from 'fs';
+import * as path from 'path';
+
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Voice Recorder' });
+});
+
+router.get('/new', (req, res, next) => {
+  res.render('index', { title: 'Voice Recorder' });
+});
+
+router.get('/license', (req, res, next) => {
+  const file = path.join(__dirname, '../', 'LICENSE.txt');
+  const text = fs.readFileSync(file);
+
+  res.header('Content-Type', 'text/plain;charset=utf-8');
+  res.send(text);
+});
+
+export default router;
