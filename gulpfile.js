f9999f1332f0570b8363a42e456d26ef42808fa4
Koji Uegaki <kjugkjk@gmail.com>
2018-03-05 10:44:40 +0900

    Add dynamic url precache.

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,23 @@ const gulp = require('gulp');
 
 gulp.task('default', function() {
   gulp.run('generate-service-worker');
-})
+});
 
 gulp.task('generate-service-worker', function(callback) {
   var swPrecache = require('sw-precache');
   var rootDir = 'public';
 
-  swPrecache.write(`${rootDir}/service-worker.js`, {
-    staticFileGlobs: [
-      rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}',
-    ],
-    stripPrefix: rootDir,
-    maximumFileSizeToCacheInBytes: 3500000
-  }, callback);
+  swPrecache.write(
+    `${rootDir}/service-worker.js`,
+    {
+      staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+      dynamicUrlToDependencies: {
+        '/': ['views/layout.pug', 'views/index.pug'],
+        '/new': ['views/layout.pug', 'views/index.pug']
+      },
+      stripPrefix: rootDir,
+      maximumFileSizeToCacheInBytes: 3500000
+    },
+    callback
+  );
 });
