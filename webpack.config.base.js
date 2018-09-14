e7e8a55d0d75bf259fa0343731b688e3a3b6cb48
Koji Uegaki <kjugkjk@gmail.com>
2018-09-14 18:25:14 +0900

    Change dir name.

--- webpack.config.base.js
+++ webpack.config.base.js
@@ -10,7 +10,7 @@ let clearOptions = {
 
 let config = {
   entry: {
-    main: './client/index.tsx'
+    main: './src/index.tsx'
   },
   output: {
     filename: '[name].[contenthash].js',
@@ -46,7 +46,7 @@ let config = {
     new CleanWebpackPlugin('dist', clearOptions),
     new HtmlWebpackPlugin({
       filename: '../index.html',
-      template: path.resolve(__dirname, './client/assets/index.html'),
+      template: path.resolve(__dirname, './src/assets/index.html'),
       minify: true
     })
   ]
