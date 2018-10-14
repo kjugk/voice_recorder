318a50767bb62077be11a35109986c57bf085904
kjugk <k02ugk@gmail.com>
2018-10-14 19:07:49 +0900

    Move some files

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -7,7 +7,7 @@ export class Footer extends React.Component {
         <div className="container">
           <h5 className="title is-5">Voice Recorder(Ver 1.0.0)</h5>
           <div className="title is-5">by <a href="https://twitter.com/kjugk1222?lang=ja" target="_blank">kjugk</a></div>
-          <div><a href="/license">MIT License</a></div>
+          <div><a href="/LICENSE.txt">MIT License</a></div>
           <div><a href="https://github.com/kjugk/voice_recorder/issues">File a bug report.</a></div>
           <div className="u-margin-m-t">
             <a href="https://bulma.io">
